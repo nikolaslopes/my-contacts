002986ef41af7aa5a91ca6145a2367f38cae68f4
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-01 02:43:28 -0400

    chore: start backend foundation

---
+++ root/backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const teste = testando;
+
+app.get("/", (request, response) => {
+	response.send("Hello world!");
+});
+
+app.listen(3000, () =>
+	console.log("🔥 Server started at http://localhost:3000"),
+);
