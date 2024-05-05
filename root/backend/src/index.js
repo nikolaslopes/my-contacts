f0f190792c2579581f6abd2a37c744ceccf6fd08
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-05 18:11:00 -0400

    feat: add routes

--- root/backend/src/index.js
+++ root/backend/src/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express')
 
-const routes = require("./routes");
+const routes = require('./routes')
 
-const app = express();
-app.use(routes);
+const app = express()
+app.use(routes)
 
 app.listen(3000, () =>
-	console.log("🔥 Server started at http://localhost:3000"),
-);
+	console.log('🔥 Server started at http://localhost:3000'),
+)
