928a8d4a1a44e5268627f59f42fde40077fea819
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-10 09:07:07 -0400

    feat: create categories repository

---
+++ root/backend/src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,26 @@
+const db = require('../../database');
+
+class CategoriesRepository {
+	async findAll() {
+		const rows = await db.query(`
+      SELECT * FROM categories
+    `);
+
+		return rows;
+	}
+
+	async create({ name }) {
+		const [row] = await db.query(
+			`
+      INSERT INTO categories(name)
+      VALUES($1)
+      RETURNING *
+    `,
+			[name],
+		);
+
+		return row;
+	}
+}
+
+module.exports = new CategoriesRepository();
