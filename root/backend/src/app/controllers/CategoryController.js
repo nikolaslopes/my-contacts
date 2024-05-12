38ba93047433a8d7405abfed412d1a106070655a
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-11 20:30:05 -0400

    feat: create update category controller

--- root/backend/src/app/controllers/CategoryController.js
+++ root/backend/src/app/controllers/CategoryController.js
@@ -14,10 +14,25 @@ class CategoryController {
 			return response.status(400).json({ error: 'Name is required' });
 		}
 
-		const category = await CategoriesRepository.create({ name });
+		const category = await CategoriesRepository.create(name);
 
 		response.json(category);
 	}
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required'});
+    }
+
+    const category = await CategoriesRepository.update(id, {
+      name
+    });
+
+    response.json(category);
+  }
 
   async delete(request, response) {
     const { id } = request.params;
