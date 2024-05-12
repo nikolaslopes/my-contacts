dbd57fa0bf9ef8691f508bc468b068d7e4f5f09e
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-11 20:56:50 -0400

    feat: create `findById` method in categories repository

--- root/backend/src/app/repositories/CategoriesRepository.js
+++ root/backend/src/app/repositories/CategoriesRepository.js
@@ -10,6 +10,17 @@ class CategoriesRepository {
 		return rows;
 	}
 
+  async findById(id) {
+    const [row] = await db.query(
+      `
+        SELECT * FROM categories
+        WHERE id = $1
+      `
+    , [id]);
+
+    return row;
+  }
+
   async findByName(name) {
     const [row] = await db.query(`
       SELECT * FROM categories
