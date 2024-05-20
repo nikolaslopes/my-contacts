351fd6953b426138c1d90ebfe7485c4386900bd2
nikolaslopes <nikolaslopes.dev@gmail.com>
2024-05-20 13:57:10 -0400

    refactor: abstract `middlewares` to recude code legibility

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 require('express-async-errors');
 
-const routes = require('./routes');
-
 const app = express();
 
-app.use(express.json());
-app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
+const routes = require('./routes');
 
-app.listen(3000, () =>
-  console.log('🔥 Server started at http://localhost:3000'),
+app.use(express.json());
+app.use(cors);
+app.use(routes);
+app.use(errorHandler);
+
+app.listen(3001, () =>
+  console.log('🔥 Server started at http://localhost:3001'),
 );
